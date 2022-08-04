425c620e7797b1dbdbf0d7e2e6dfe52451938b94
eisont <rlaclgns321@naver.com>
2022-08-04 11:01:39 +0900

    220804 네비게이션 바 수정

--- freeboard_frontend/src/components/commons/layout/navigation/index.tsx
+++ freeboard_frontend/src/components/commons/layout/navigation/index.tsx
@@ -6,6 +6,15 @@ import * as S from "./Navigation.styles";
 export default function LayoutNavigation() {
   const router = useRouter();
 
+  const Board = ["/boards"];
+  const BoardNew = ["/boards/new"];
+  const isBoard = Board.includes(router.asPath);
+  const isBoardNew = BoardNew.includes(router.asPath);
+  const Markets = ["/markets"];
+  const MarketsNew = ["/markets/new"];
+  const isMarkets = Markets.includes(router.asPath);
+  const isMarketsNew = MarketsNew.includes(router.asPath);
+
   const onClickBoard = () => {
     router.push("/boards");
   };
@@ -18,16 +27,24 @@ export default function LayoutNavigation() {
   const onClickMoveToMarketNew = () => {
     router.push("/markets/new");
   };
+  const onClickMoveToBoardNew = () => {
+    router.push("/boards/new");
+  };
 
   return (
     <S.Wrapper>
       <S.BtBox>
         <S.Page>
           <S.PageBt onClick={onClickBoard}>Board</S.PageBt>
-          <S.PageBt onClick={onClickMarket}>Used Market</S.PageBt>
+          <S.PageBt onClick={onClickMarket}>Market</S.PageBt>
           <S.PageBt onClick={onClickMypage}>My-Page</S.PageBt>
         </S.Page>
-        <S.WriteBt onClick={onClickMoveToMarketNew}>상품 등록</S.WriteBt>
+        {(isBoardNew || isBoard) && (
+          <S.WriteBt onClick={onClickMoveToBoardNew}>게시판 등록</S.WriteBt>
+        )}
+        {(isMarketsNew || isMarkets) && (
+          <S.WriteBt onClick={onClickMoveToMarketNew}>상품 등록</S.WriteBt>
+        )}
       </S.BtBox>
     </S.Wrapper>
   );
