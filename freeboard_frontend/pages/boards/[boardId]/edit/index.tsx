1c269f23f1d944e70abb9954daa06acf3165e663
힐링사운드 <bibi@HealingSound-iMac.local>
2022-12-08 13:52:06 +0900

    boardUpdate 수정 merge

--- freeboard_frontend/pages/boards/[boardId]/edit/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/edit/index.tsx
@@ -12,23 +12,18 @@ const FETCH_BOARD = gql`
       title
       contents
       youtubeUrl
-      likeCount
-      dislikeCount
       images
       boardAddress {
         zipcode
         address
         addressDetail
       }
-      images
       createdAt
-      updatedAt
-      deletedAt
     }
   }
 `;
 
-export default function BoardsEditPage() {
+const BoardsEditPage = () => {
   // 기능
   const router = useRouter();
   // 수정하기에서 사용하는 조회기능
@@ -37,4 +32,6 @@ export default function BoardsEditPage() {
   });
 
   return <BoardWrite isEdit={true} fetchBoardData={fetchBoardData} />;
-}
+};
+
+export default BoardsEditPage;
